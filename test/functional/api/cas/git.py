c98b7e1bb35a3a30456334b101175a1fe62d8ea3
Robert Baldyga <robert.baldyga@intel.com>
2019-10-29 18:18:06 +0100

    Use git utils from cas API
    Test framework does no longer provide git_utils, so we switch
    to local implementation.
    
    Signed-off-by: Robert Baldyga <robert.baldyga@intel.com>

---
+++ test/functional/api/cas/git.py
@@ -0,0 +1,21 @@
+#
+# Copyright(c) 2019 Intel Corporation
+# SPDX-License-Identifier: BSD-3-Clause-Clear
+#
+
+from core.test_run import TestRun
+from connection.local_executor import LocalExecutor
+
+
+def get_current_commit_hash():
+    local_executor = LocalExecutor()
+    return local_executor.run(
+        f"cd {TestRun.plugins['opencas']['repo_dir']} &&"
+        f'git show HEAD --pretty=format:"%H"').stdout
+
+
+def get_current_commit_message():
+    local_executor = LocalExecutor()
+    return local_executor.run(
+        f"cd {TestRun.plugins['opencas']['repo_dir']} &&"
+        f'git show HEAD --pretty=format:"%s"').stdout
