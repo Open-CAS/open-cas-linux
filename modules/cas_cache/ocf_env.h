aec95d70c99791afa4eff2fd944ab61c61b20b4e
Adam Rutkowski <adam.j.rutkowski@intel.com>
2020-02-10 16:25:35 -0500

    Add ENV_BUILD_BUG_ON macro to kernel environment
    Signed-off-by: Adam Rutkowski <adam.j.rutkowski@intel.com>

--- modules/cas_cache/ocf_env.h
+++ modules/cas_cache/ocf_env.h
@@ -621,6 +621,7 @@ static inline uint32_t env_crc32(uint32_t crc, uint8_t const *data, size_t len)
 
 #define ENV_BUG()			BUG()
 #define ENV_BUG_ON(cond)		BUG_ON(cond)
+#define ENV_BUILD_BUG_ON(cond)		BUILD_BUG_ON(cond)
 
 
 /* *** EXECUTION COTNEXT *** */
