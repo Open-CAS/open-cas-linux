8def35c98661175103b2af19b86449ae7a431a6b
Michal Mielewczyk <michal.mielewczyk@intel.com>
2020-05-25 09:15:27 -0400

    test api: wrapper for retrieving CAS release tags
    Signed-off-by: Michal Mielewczyk <michal.mielewczyk@intel.com>

--- test/functional/api/cas/git.py
+++ test/functional/api/cas/git.py
@@ -3,6 +3,8 @@
 # SPDX-License-Identifier: BSD-3-Clause-Clear
 #
 
+import os
+
 from core.test_run import TestRun
 from connection.local_executor import LocalExecutor
 
@@ -19,3 +21,13 @@ def get_current_commit_message():
     return local_executor.run(
         f"cd {TestRun.usr.repo_dir} &&"
         f'git show HEAD -s --pretty=format:"%B"').stdout
+
+
+def get_release_tags():
+    repo_path = os.path.join(TestRun.usr.working_dir, ".git")
+    output = TestRun.executor.run_expect_success(f"git --git-dir={repo_path} tag").stdout
+
+    # Tags containing '-' or '_' are not CAS release versions
+    tags = [v for v in output.splitlines() if "-" not in v and "_" not in v]
+
+    return tags
