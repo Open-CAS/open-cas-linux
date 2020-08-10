91b678f3e29950a64f8f9e6f58e5d4f4284ca995
Slawomir Jankowski <slawomir.jankowski@intel.com>
2020-08-10 18:50:01 +0200

    Data integrity test with many cores with high-low pattern
    Signed-off-by: Slawomir Jankowski <slawomir.jankowski@intel.com>

--- test/functional/tests/data_integrity/test_data_integrity_with_patterns.py
+++ test/functional/tests/data_integrity/test_data_integrity_with_patterns.py
@@ -94,6 +94,68 @@ def test_data_integrity_single_pattern(cache_mode, pattern):
             core.unmount()
 
 
+@pytest.mark.os_dependent
+@pytest.mark.parametrize("cache_mode", CacheMode)
+@pytest.mark.require_disk("cache", DiskTypeSet([DiskType.optane, DiskType.nand]))
+@pytest.mark.require_disk("core", DiskTypeLowerThan("cache"))
+def test_data_integrity_hi_lo_pattern(cache_mode):
+    """
+        title:
+          Data integrity test with high-low write pattern with 2 hours duration time
+        description: |
+          Run fio with high-low pattern (twice - first time high, second time low) on each core
+          and verify if there are no write errors.
+        pass_criteria:
+            - System does not crash.
+            - Fio does not return any errors.
+    """
+    with TestRun.step("Prepare cache and core."):
+        cache_dev = TestRun.disks['cache']
+        cache_dev.create_partitions([exp_obj_size * (exp_obj_number / 4)])
+        cache_part = cache_dev.partitions[0]
+        core_dev = TestRun.disks['core']
+        core_dev.create_partitions([exp_obj_size] * exp_obj_number)
+
+    with TestRun.step(f"Start cache in {cache_mode} mode"):
+        cache = casadm.start_cache(cache_part, cache_mode, force=True)
+
+    with TestRun.step(f"Add all core devices to cache."):
+        cores = []
+        for core_part in core_dev.partitions:
+            core_part.create_filesystem(Filesystem.ext3, True)
+            cores.append(cache.add_core(core_part))
+
+    with TestRun.step("Mount all cores"):
+        fio_targets = []
+        for i, core in enumerate(cores):
+            core_mount_point = f"{mount_point}{cache.cache_id}-{core.core_id}"
+            core.mount(f"{core_mount_point}")
+            fio_targets.append(f"{core_mount_point}/test_file")
+
+    with TestRun.step(f"Run fio with high-low pattern."):
+        fio_high = prepare_unified_fio(fio_targets, Pattern.high)
+        fio_high_output = fio_high.run()
+        fio_low = prepare_unified_fio(fio_targets, Pattern.low)
+        fio_low_output = fio_low.run()
+
+    with TestRun.step("Check for fio errors."):
+        fio_errors = fio_high_output[0].total_errors()
+        if fio_errors:
+            TestRun.LOGGER.error(f"Found {fio_errors} errors in fio output")
+
+        fio_errors = fio_low_output[0].total_errors()
+        if fio_errors:
+            TestRun.LOGGER.error(f"Found {fio_errors} errors in fio output")
+
+    with TestRun.step("Delete test files."):
+        for file in fio_targets:
+            remove(file, True, True, True)
+
+    with TestRun.step("Unmount all cores."):
+        for core in cores:
+            core.unmount()
+
+
 def prepare_unified_fio(targets, data_pattern):
     fio = (
         _prepare_fio()
