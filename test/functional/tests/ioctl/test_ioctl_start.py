eb83431df4857f36e0a27c9c5ef4ad802efe054a
Slawomir Jankowski <slawomir.jankowski@intel.com>
2021-02-02 14:46:25 +0100

    Add ioctl test for starting cache
    Signed-off-by: Slawomir Jankowski <slawomir.jankowski@intel.com>

---
+++ test/functional/tests/ioctl/test_ioctl_start.py
@@ -0,0 +1,65 @@
+#
+# Copyright(c) 2021 Intel Corporation
+# SPDX-License-Identifier: BSD-3-Clause-Clear
+#
+
+import pytest
+from time import sleep
+
+from api.cas.casadm import start_cache
+from api.cas.casadm_parser import get_caches
+from core.test_run import TestRun
+from storage_devices.disk import DiskType, DiskTypeSet
+from test_tools.cas_ioctl.cas_requests import StartCacheRequest
+from test_tools.cas_ioctl.cas_structs import CacheMode, CacheLineSize, InitCache
+from test_tools.cas_ioctl.ioctl import cas_ioctl
+from test_utils.size import Size, Unit
+from tests.ioctl import common_utils
+
+cache_id = 3
+
+
+@pytest.mark.parametrizex("force", [0, 1])
+@pytest.mark.parametrizex("load", InitCache)
+@pytest.mark.parametrizex("cache_mode", CacheMode)
+@pytest.mark.parametrizex("cache_line_size", CacheLineSize)
+@pytest.mark.require_disk("cache", DiskTypeSet([DiskType.optane, DiskType.nand]))
+def test_ioctl_start(cache_mode, cache_line_size, load, force):
+    """
+        title: Start the cache without casadm.
+        description: |
+          Test of the ability to start the cache with IOCTL request bypassing native
+          OpenCAS manager - casadm.
+        pass_criteria:
+          - Cache started successfully without any errors.
+          - When 'force' and 'load' flags are used cache is not started.
+    """
+    with TestRun.step("Prepare cache device."):
+        cache_dev = TestRun.disks['cache']
+        cache_dev.create_partitions([Size(2, Unit.GiB)])
+        cache_part = cache_dev.partitions[0]
+
+    with TestRun.step("Create IOCTL request for cache start."):
+        start_config = StartCacheRequest(
+            cache_path_name=cache_part.path, caching_mode=cache_mode, cache_id=cache_id,
+            init_cache=load, force=force, line_size=cache_line_size
+        )
+
+    if load.value:
+        with TestRun.step("Start and stop cache before loading."):
+            cache = start_cache(cache_part, cache_id=cache_id, force=True)
+            cache.stop()
+
+    with TestRun.step("Start cache with IOCTL request bypassing casadm."):
+        cas_ioctl(start_config)
+
+    with TestRun.step(
+            f"Check if cache is {'running' if not (load.value and force) else 'not running'}."
+    ):
+        if not (load.value and force):
+            if len(get_caches()) != 1:
+                TestRun.fail("Cache is missing!")
+        else:
+            if len(get_caches()) != 0:
+                TestRun.fail("Cache should not be loaded!")
+
