cc2ddc1b57050088fc4ef2d901ce9c8d521110a0
Slawomir Jankowski <slawomir.jankowski@intel.com>
2021-02-02 14:46:53 +0100

    Add ioctl test for interrupting cache start
    Signed-off-by: Slawomir Jankowski <slawomir.jankowski@intel.com>

--- test/functional/tests/ioctl/test_ioctl_start.py
+++ test/functional/tests/ioctl/test_ioctl_start.py
@@ -63,3 +63,50 @@ def test_ioctl_start(cache_mode, cache_line_size, load, force):
             if len(get_caches()) != 0:
                 TestRun.fail("Cache should not be loaded!")
 
+
+@pytest.mark.parametrizex("force", [0, 1])
+@pytest.mark.parametrizex("load", InitCache)
+@pytest.mark.parametrizex("cache_mode", CacheMode)
+@pytest.mark.parametrizex("cache_line_size", CacheLineSize)
+@pytest.mark.require_disk("cache", DiskTypeSet([DiskType.optane, DiskType.nand]))
+def test_ioctl_start_interrupt(cache_mode, cache_line_size, load, force):
+    """
+        title: Interrupt cache start without casadm.
+        description: |
+          Negative test of the ability to interrupt the cache start with IOCTL request sent
+          outside native OpenCAS manager - casadm.
+        pass_criteria:
+          - Cache is not started.
+    """
+    with TestRun.step("Prepare cache device."):
+        cache_dev = TestRun.disks['cache']
+        cache_dev.create_partitions([Size(128, Unit.GiB)])
+        cache_part = cache_dev.partitions[0]
+
+    with TestRun.step("Create IOCTL request for cache start."):
+        start_config = StartCacheRequest(
+            cache_path_name=cache_part.path, caching_mode=cache_mode, cache_id=cache_id,
+            init_cache=load, force=force, line_size=cache_line_size
+        )
+
+    if load.value:
+        with TestRun.step("Start and stop cache before loading."):
+            cache = start_cache(cache_part, cache_id=cache_id, force=True)
+            cache.stop()
+
+    with TestRun.step("Clear dmesg."):
+        common_utils.clear_dmesg()
+
+    with TestRun.step("Interrupt starting cache with IOCTL request bypassing casadm."):
+        cas_ioctl(start_config, True)
+        sleep(8)    # wait for rollback after interruption
+
+    with TestRun.step(f"Check if cache is not running."):
+        if len(get_caches()) > 0:
+            TestRun.fail('Start process finished. Interruption failed.')
+        else:
+            TestRun.LOGGER.info('Start process is dead as expected.')
+
+    with TestRun.step("Check dmesg for interruption log."):
+        common_utils.check_dmesg(common_utils.load_and_force if (load.value and force)
+                                 else common_utils.interrupt_start)
