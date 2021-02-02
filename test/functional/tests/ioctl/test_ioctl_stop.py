9c2a692ec2356ca90c1387e3fa15cd9657536a59
Slawomir Jankowski <slawomir.jankowski@intel.com>
2021-02-02 14:47:33 +0100

    Add ioctl test for interrupting clean cache stop
    Signed-off-by: Slawomir Jankowski <slawomir.jankowski@intel.com>

--- test/functional/tests/ioctl/test_ioctl_stop.py
+++ test/functional/tests/ioctl/test_ioctl_stop.py
@@ -47,3 +47,42 @@ def test_ioctl_stop_clean_cache(flush_data):
         if len(get_caches()) != 0:
             TestRun.fail("Cache should be stopped despite the interruption!")
 
+
+@pytest.mark.parametrizex("flush_data", [0, 1])
+@pytest.mark.require_disk("cache", DiskTypeSet([DiskType.optane, DiskType.nand]))
+def test_ioctl_stop_interrupt_clean_cache(flush_data):
+    """
+        title: Interrupt cache stop without casadm.
+        description: |
+          Negative test of the ability to interrupt the cache stop with IOCTL request sent
+          outside native OpenCAS manager - casadm. The cache is clean and no flush can be
+          interrupted during stop.
+        pass_criteria:
+          - Cache is stopped.
+    """
+    with TestRun.step("Prepare cache device."):
+        cache_dev = TestRun.disks['cache']
+        cache_dev.create_partitions([Size(128, Unit.GiB)])
+        cache_part = cache_dev.partitions[0]
+
+    with TestRun.step("Create IOCTL request for cache stop."):
+        stop_config = StopCacheRequest(cache_id=cache_id, flush_data=flush_data)
+
+    with TestRun.step("Start cache before stopping."):
+        start_cache(cache_part, cache_id=cache_id, force=True)
+
+    with TestRun.step("Clear dmesg."):
+        common_utils.clear_dmesg()
+
+    with TestRun.step("Interrupt stopping cache with IOCTL request bypassing casadm."):
+        cas_ioctl(stop_config, True)
+        sleep(8)    # wait for thread to finish asynchronously after interruption
+
+    with TestRun.step(f"Check if cache is not running."):
+        if len(get_caches()) > 0:
+            TestRun.fail("Cache should be stopped despite the interruption!")
+        else:
+            TestRun.LOGGER.info('Stop process finished as expected.')
+
+    with TestRun.step("Check dmesg for interruption log."):
+        common_utils.check_dmesg(common_utils.interrupt_stop)
