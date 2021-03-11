5e035f4b52927238e7821b53c01ed6a0cae1c5a2
Michal Mielewczyk <michal.mielewczyk@intel.com>
2021-03-11 09:37:37 +0100

    casadm: improve error logging
    Signed-off-by: Michal Mielewczyk <michal.mielewczyk@intel.com>

--- casadm/extended_err_msg.c
+++ casadm/extended_err_msg.c
@@ -158,6 +158,10 @@ struct {
 		OCF_ERR_INVAL_CACHE_DEV,
 		"Device does not meet requirements."
 	},
+	{
+		OCF_ERR_NO_LOCK,
+		"Failed to lock cache for management operation."
+	},
 
 	/* CAS kernel error mappings*/
 	{
