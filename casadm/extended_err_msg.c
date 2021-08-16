07ba79c57c5ac80b09d998015788e7a58d711692
Michal Mielewczyk <michal.mielewczyk@intel.com>
2021-08-16 16:02:09 +0200

    Remove remains of upgrade-in-flight support
    Signed-off-by: Michal Mielewczyk <michal.mielewczyk@intel.com>

--- casadm/extended_err_msg.c
+++ casadm/extended_err_msg.c
@@ -205,10 +205,6 @@ struct {
 		"logical sector size as core device."
 	},
 	{
-		KCAS_ERR_ROLLBACK,
-		"Cannot restore previous configuration"
-	},
-	{
 		KCAS_ERR_CONTAINS_PART,
 		"Device contains partitions.\nIf you want to continue, "
 		"please use --force option.\nWarning: all data will be lost!"
