c9ce42c57f80ceb44328c0adbc80319ae86bd7f7
Arkadiusz Neumann <arkadiusz.neumann@intel.com>
2022-04-08 14:22:35 +0200

    add workflow with copyright date verification on pull request events

---
+++ .github/verify_header.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#
+# Copyright(c) 2022 Intel Corporation
+# SPDX-License-Identifier: BSD-3-Clause
+#
+
+YEAR=$(date +"%Y")
+REGEX="Copyright\(c\) [0-9]{4}-([0-9]{4}) |Copyright\(c\) ([0-9]{4}) "
+
+while read -r line; do
+    if [[ "$line" =~ $REGEX ]]; then
+        echo ${BASH_REMATCH[0]}
+        if [[ $YEAR == ${BASH_REMATCH[1]} || $YEAR == ${BASH_REMATCH[2]} ]]; then
+            echo $1 have appropriate license header
+            exit 0
+        fi
+        continue
+    fi
+done < "$1"
+
+ echo $1 have wrong license header year
+exit 1
