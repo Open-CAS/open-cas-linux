0016a0e31f99d256079067d1439e5d5398faf309
Katarzyna Treder <katarzyna.treder@h-partners.com>
2024-08-22 14:34:58 +0200

    Refactor common fuzzy files
    Signed-off-by: Katarzyna Treder <katarzyna.treder@h-partners.com>

--- test/functional/tests/security/fuzzy/kernel/fuzzy_with_io/common/common.py
+++ test/functional/tests/security/fuzzy/kernel/fuzzy_with_io/common/common.py
@@ -1,5 +1,6 @@
 #
 # Copyright(c) 2022 Intel Corporation
+# Copyright(c) 2024 Huawei Technologies
 # SPDX-License-Identifier: BSD-3-Clause
 #
 from datetime import timedelta
@@ -8,19 +9,23 @@ from test_tools.fio.fio import Fio
 from test_tools.fio.fio_param import IoEngine, ReadWrite
 from test_utils.size import Size, Unit
 
+mount_point = "/mnt/test"
+
 
 def get_basic_workload(mount_point: str):
     file_min_size = Size(10, Unit.Byte).get_value()
     file_max_size = Size(512, Unit.KiB).get_value()
-    fio = (Fio()
-           .create_command()
-           .io_engine(IoEngine.libaio)
-           .direct()
-           .run_time(timedelta(days=1))
-           .time_based()
-           .directory(mount_point)
-           .read_write(ReadWrite.randrw)
-           .nr_files(1000)
-           .file_size_range([(file_min_size, file_max_size)])
-           .num_jobs(32))
+    fio = (
+        Fio()
+        .create_command()
+        .io_engine(IoEngine.libaio)
+        .direct()
+        .run_time(timedelta(days=1))
+        .time_based()
+        .directory(mount_point)
+        .read_write(ReadWrite.randrw)
+        .nr_files(1000)
+        .file_size_range([(file_min_size, file_max_size)])
+        .num_jobs(32)
+    )
     return fio
