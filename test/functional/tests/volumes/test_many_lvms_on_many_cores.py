9c63594062ea972a7a6ce0d67480ab3d1818c6cd
Kamil Gierszewski <kamil.gierszewski@huawei.com>
2025-04-15 11:09:54 +0200

    tests: remove cleanup in test
    Signed-off-by: Kamil Gierszewski <kamil.gierszewski@huawei.com>

--- test/functional/tests/volumes/test_many_lvms_on_many_cores.py
+++ test/functional/tests/volumes/test_many_lvms_on_many_cores.py
@@ -1,6 +1,6 @@
 #
 # Copyright(c) 2022 Intel Corporation
-# Copyright(c) 2024 Huawei Technologies Co., Ltd.
+# Copyright(c) 2024-2025 Huawei Technologies Co., Ltd.
 # SPDX-License-Identifier: BSD-3-Clause
 #
 
@@ -111,7 +111,3 @@ def test_many_lvms_on_many_cores():
 
     with TestRun.step("Run FIO with verification on LVM."):
         fio_run.run()
-
-    with TestRun.step("Remove LVMs and clean up config changes."):
-        Lvm.remove_all()
-        LvmConfiguration.remove_filters_from_config()
