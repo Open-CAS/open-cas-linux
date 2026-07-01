37925d929324a1f1023fad8cd86d79d6db419d8f
Robert Baldyga <robert.baldyga@unvertical.com>
2026-07-01 13:30:51 +0200

    tests: Add recovery unplug tests for metadata error mode
    Signed-off-by: Robert Baldyga <robert.baldyga@unvertical.com>

--- test/functional/tests/lazy_writes/recovery/test_recovery_unplug.py
+++ test/functional/tests/lazy_writes/recovery/test_recovery_unplug.py
@@ -12,6 +12,7 @@ from api.cas import casadm, cli_messages
 from api.cas.cache_config import CacheMode, CacheModeTrait, CacheLineSize
 from core.test_run import TestRun
 from storage_devices.disk import DiskTypeSet, DiskType, DiskTypeLowerThan
+from test_tools.dd import Dd
 from test_tools.fs_tools import Filesystem
 from connection.utils.output import CmdException
 from type_def.size import Size, Unit
@@ -129,7 +130,8 @@ def test_recovery_unplug_cache_raw(cache_mode, cls):
             title: Test for recovery after cache drive removal - test on raw device.
             description: |
               Verify that unflushed data can be safely recovered after, when SSD drive is removed
-              after write completion - test on raw device.
+              after write completion - test on raw device. Stop immediately after unplug to verify
+              flush error handling.
             pass_criteria:
               - CAS recovers successfully after cache drive unplug
               - No data corruption
@@ -160,6 +162,96 @@ def test_recovery_unplug_cache_raw(cache_mode, cls):
         TestRun.LOGGER.info(f"List caches:\n{casadm.list_caches().stdout}")
         TestRun.LOGGER.info(f"Dirty blocks on cache: "
                             f"{cache.get_dirty_blocks().get_value(Unit.Blocks4096)}")
+
+    with TestRun.step("Stop cache."):
+        try:
+            cache.stop()
+            TestRun.fail("Stopping the cache should be aborted without --no-flush flag.")
+        except CmdException as e:
+            TestRun.LOGGER.info(str(e.output))
+            try:
+                cache.stop(no_data_flush=True)
+                TestRun.LOGGER.warning("Expected stopping cache with errors with --no-flush flag.")
+            except CmdException as e1:
+                cli_messages.check_stderr_msg(e1.output, cli_messages.stop_cache_errors)
+
+    with TestRun.step("Plug missing cache device."):
+        TestRun.LOGGER.info(str(casadm.list_caches(by_id_path=False)))
+        cache_disk.plug_all()
+
+    with TestRun.step("Load cache."):
+        cache = casadm.load_cache(cache_device)
+        TestRun.LOGGER.info(f"Dirty blocks on cache: "
+                            f"{cache.get_dirty_blocks().get_value(Unit.Blocks4096)}")
+
+    with TestRun.step("Stop cache with data flush."):
+        cache.stop()
+
+    with TestRun.step("Copy file from core device and check md5sum."):
+        copy_file(source=core_device.path, target=target_file.full_path,
+                  size=test_file_size, direct="iflag")
+        target_file_md5 = target_file.md5sum()
+        compare_files(source_file_md5, target_file_md5)
+
+    with TestRun.step("Cleanup core device and remove test files."):
+        try:
+            target_file.remove()
+            source_file.remove()
+        except Exception:
+            # On some OSes files at /tmp location are automatically removed after DUT hard reset
+            pass
+
+
+@pytest.mark.require_disk("cache", DiskTypeSet([DiskType.optane, DiskType.nand]))
+@pytest.mark.require_disk("core", DiskTypeLowerThan("cache"))
+@pytest.mark.parametrizex("cache_mode", CacheMode.with_traits(CacheModeTrait.LazyWrites))
+@pytest.mark.parametrizex("cls", [CacheLineSize.LINE_4KiB, CacheLineSize.LINE_64KiB])
+@pytest.mark.require_plugin("power_control")
+def test_recovery_unplug_cache_raw_metadata_err(cache_mode, cls):
+    """
+            title: Test for recovery after cache drive removal - test on raw device.
+            description: |
+              Verify that unflushed data can be safely recovered after, when SSD drive is removed
+              after write completion - test on raw device. Trigger cache error by write attempt
+              after unplugging cache, to enter metadata error mode before stop.
+            pass_criteria:
+              - CAS recovers successfully after cache drive unplug
+              - No data corruption
+    """
+    with TestRun.step("Prepare devices"):
+        cache_disk = TestRun.disks['cache']
+        core_disk = TestRun.disks['core']
+        cache_disk.create_partitions([Size(2, Unit.GibiByte)])
+        core_disk.create_partitions([Size(16, Unit.GibiByte)])
+        cache_device = cache_disk.partitions[0]
+        core_device = core_disk.partitions[0]
+
+    with TestRun.step("Create test files."):
+        source_file, target_file = create_test_files(test_file_size)
+        source_file_md5 = source_file.md5sum()
+
+    with TestRun.step("Start cache and add core."):
+        cache = casadm.start_cache(cache_device, cache_mode, cls, force=True)
+        core = cache.add_core(core_device)
+
+    with TestRun.step("Copy file to CAS."):
+        copy_file(source=source_file.full_path, target=core.path,
+                  size=test_file_size, direct="oflag")
+        TestRun.LOGGER.info(str(core.get_statistics()))
+
+    with TestRun.step("Unplug cache device."):
+        cache_disk.unplug()
+        TestRun.LOGGER.info(f"List caches:\n{casadm.list_caches().stdout}")
+        TestRun.LOGGER.info(f"Dirty blocks on cache: "
+                            f"{cache.get_dirty_blocks().get_value(Unit.Blocks4096)}")
+
+    with TestRun.step("Try to write to CAS."):
+        Dd().input("/dev/random") \
+            .output(core.path) \
+            .oflag("direct") \
+            .block_size(Size(1, Unit.Blocks4096)) \
+            .count(1) \
+            .run()
 
     with TestRun.step("Stop cache."):
         try:
